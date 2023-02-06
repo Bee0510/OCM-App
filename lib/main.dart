1d88052143a30b0779960ebb323ea9c9cc0f632e
Bee0510 <beheravishal19@gmail.com>
2023-02-07 03:43:15 +0530

    some_unused_items_removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names
 
-import 'package:firebase/Authenticate/Register.dart';
-import 'package:firebase/Authenticate/SignIN.dart';
 import 'package:firebase/Models/User.dart';
 import 'package:firebase/MainPage.dart';
 import 'package:firebase/home/Wrapper.dart';
@@ -29,9 +27,6 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.pink,
         ),
         home: Wrapper(),
-        routes: {
-          'SecondPage': (context) => SecondPage(),
-        },
       ),
     );
   }
